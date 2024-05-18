55ace1b5a6a6c266b12bd7520838f4b3dd70fe8b
Joaquin Otalora <104650875+Jolx77@users.noreply.github.com>
2024-05-18 16:04:33 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 as -g -o Protected_mode.o Protected_mode.S
 ld --oformat binary -o main.img -T link.ld Protected_mode.o
-qemu-system-i386 -hda main.img -boot a -s -S monitor stdio
+qemu-system-i386 -hda main.img -boot a -s -S -monitor stdio
 
